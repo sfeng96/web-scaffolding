96ad9e816a7044463ad5a7e1bb3b9053d0e04918
Robert Feng <shike960113@gmail.com>
2021-03-23 23:09:58 +0000

    added webflux basic rest

---
+++ webflux/src/main/java/learning/scaffolding/web/webflux/WebfluxApplication.java
@@ -0,0 +1,12 @@
+package learning.scaffolding.web.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebfluxApplication.class, args);
+  }
+}
